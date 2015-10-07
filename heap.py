d974d5b0763824cb97f3e307ebd97ac24a7c4a4e
Unathi Chonco <choncou@gmail.com>
2015-10-06 22:42:30 -0700

    Tokenize the corpus and store key-frequncy in heap

---
+++ heap.py
@@ -0,0 +1,71 @@
+import math
+
+
+class Heap:
+    def __init__(self):
+        self.heap = [Node("empty", 0)]
+
+    def insert(self, key, val):
+        new_node = Node(key, val)
+        self.heap.append(new_node)
+        pos = self.size()
+
+        self.shift_up(pos)
+        # while (pos > 1):
+        #     parent_pos = math.floor(pos/2)
+        #     if self.heap[pos].node_val > self.heap[parent_pos].node_val:
+        #         temp_node = self.heap[parent_pos]
+        #         self.heap[parent_pos] = self.heap[pos]
+        #         self.heap[pos] = temp_node
+        #         pos = parent_pos
+        #         print(self.heap[pos].node_key + " Moved up")
+        #     else:
+        #         break
+
+    def peek(self):
+        # for i in range(1, self.size()+1):
+        #     print(self.heap[i].node_val)
+        return self.heap[1]
+
+    def size(self):
+        return len(self.heap)-1
+
+    def shift_up(self, pos):
+        while (pos > 1):
+            parent_pos = math.floor(pos/2)
+            if self.heap[pos].node_val > self.heap[parent_pos].node_val:
+                temp_node = self.heap[parent_pos]
+                self.heap[parent_pos] = self.heap[pos]
+                self.heap[pos] = temp_node
+                pos = parent_pos
+                # print(self.heap[pos].node_key + " Moved up")
+            else:
+                break
+
+    def delete_max(self):
+        for i in range(1, self.size()):
+            self.heap[i] = self.heap[i+1]
+        self.heap.pop()
+        for i in range(1, self.size()):
+            self.shift_up(i)
+
+
+class Node:
+    def __init__(self, key, val):
+        self.node_key = key
+        self.node_val = val
+
+if __name__ == '__main__':
+    counts = Heap()             # create a new heap
+    counts.insert("the", 9)
+    counts.insert("dog", 7)
+    counts.insert("apple", 11)
+    counts.insert("red", 3)
+    counts.insert("ate", 1)
+    counts.insert("anoth", 8)
+    counts.insert("shake", 10)
+    counts.insert("ate", 10)
+
+    print(counts.peek().node_key)          # => (5, "red")
+    counts.delete_max()         # => (5, "red")
+    print(counts.peek().node_key)
--- tokenization.py
+++ tokenization.py
@@ -1,15 +1,44 @@
 import sys
 import re
+import random
 import token_hash_table
+import heap
 
 
 def tokenize(filename):
     file_source = open(filename, encoding='utf-8')
     big_word_list = file_source.read()
 
-    regex = re.compile('^(\".+\"|[^\s]+)')
-    sub = regex.sub(' ', big_word_list).split()
+    regex = re.compile('(\".+\"|[^\s]+)')
+    sub = re.findall(regex, big_word_list)
     return sub
+
+
+def stochastic(histo):
+    count = 0
+    val_list = histo.values()
+    key_list = histo.keys()
+    stoch_histo = token_hash_table.MyHash(5000)
+    for i in range(len(key_list)):
+        count += val_list[i]
+    for j in range(len(key_list)):
+        stoch_histo.set(key_list[j], val_list[j]/count)
+
+    return stoch_histo
+
+
+def sentence(histo, length):
+    sent = []
+    for i in range(0, length):
+        cur_rand_value = random.random()
+        cum_sum = 0
+        for k in range(len(histo.keys())):
+            cum_sum += histo.values()[k]
+            if(cum_sum >= cur_rand_value):
+                sent.append(histo.keys()[k])
+                break
+
+    return sent
 
 if __name__ == '__main__':
     token_list = []
@@ -26,4 +55,24 @@ if __name__ == '__main__':
         else:
             my_hash.update(token)
 
-    print(my_hash.values())
+    stoch_histo = stochastic(my_hash)
+    new_keys = stoch_histo.keys()
+    new_freq = stoch_histo.values()
+
+    my_heap = heap.Heap()
+
+    for i in range(len(new_keys)):
+        my_heap.insert(new_keys[i], new_freq[i])
+
+    if(len(sys.argv)) > 2:
+        for i in range(int(sys.argv[2])):
+            print(my_heap.peek().node_key)
+            my_heap.delete_max()
+    else:
+        large = input("Enter n max tokens: ")
+        for i in range(int(large)):
+            print(my_heap.peek().node_key)
+            my_heap.delete_max()
+    # sent = sentence(stoch_histo, 7)
+    # print(" ".join(sent))
+    # print(sentence(stochastic(my_hash), 1))
